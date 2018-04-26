9b08cb56f546973694e5bfee5d9da7ea375fb974
robin2005 <57048685@qq.com>
2018-04-26 10:46:48 +0800

    添加工程文件
    添加工程文件

---
+++ FBMovableButton.h
@@ -0,0 +1,13 @@
+//
+//  FBMovableButton.h
+//  FlowBank
+//
+//  Created by robin on 2017/6/8.
+//  Copyright © 2017年 irobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBMovableButton : UIButton
+
+@end
